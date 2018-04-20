c7f8d122817aa7e84b82dc3147afb4284442c4e0
Madeline Carson <madeline.carson@gmail.com>
2018-04-20 15:38:46 -0700

    Create Parties migration with basic attributes
    - Only the name is required, all other columns are optional

---
+++ priv/repo/migrations/20180420222435_create_party.exs
@@ -0,0 +1,16 @@
+defmodule SecretSantaApi.Repo.Migrations.CreateParty do
+  use Ecto.Migration
+
+  def change do
+    create table(:parties) do
+      add :name, :string, null: false
+      add :theme, :string
+      add :spending_limit, :integer
+      add :passphrase, :string
+      add :date, :naive_datetime
+      add :location, :string
+
+      timestamps()
+    end
+  end
+end
